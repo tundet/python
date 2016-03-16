81ef3200995ec08ebc0fef23ac874813fbf65191
ryunde <ryunde.taba@gmail.com>
2016-03-16 13:28:47 +0200

    variable

---
+++ 2.1.py
@@ -0,0 +1,6 @@
+# -*- coding: cp1252 -*-
+
+# create a variable and assign it a string value
+variable1 = "string-type content"
+
+print("Our variable has a value which is ",variable1,". Isn't that nice?",sep = "")

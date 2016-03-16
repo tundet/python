be4c7cfd697811eaf91316bb2fdb3bf0e334b32d
ryunde <ryunde.taba@gmail.com>
2016-03-16 13:36:46 +0200

    several lines

---
+++ 2.4.py
@@ -0,0 +1,9 @@
+# -*- coding: cp1252 -*-
+
+#Muotoilut
+charstring = """This here is divided to several lines:
+I am still in the same print-command."""
+
+print(charstring)
+
+print("\tName:\tPeter\n\tJob:\tBabysitter")

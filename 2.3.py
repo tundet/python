ba6e60fd5aae81228267b7a2255869d9685d8c54
ryunde <ryunde.taba@gmail.com>
2016-03-16 13:34:17 +0200

    tyyppimuunnokset

---
+++ 2.3.py
@@ -0,0 +1,11 @@
+# -*- coding: cp1252 -*-
+
+#Tyyppimuunnokset
+var_1 = 10.6411
+var_2 = "Stringline!"
+
+var_1 = int(var_1)
+var_2 = var_2 * 2
+
+print("Integer conversion cannot do roundings:", var_1)
+print("Multiplying strings also causes trouble:", var_2)
